0b90c4ea0364485ab4e9ce79ef14018a449058df
carlos2023110055-sudo <carlos.2023110055@metropoli.edu.mx>
2026-02-27 12:25:46 -0600

    Update editar.php
    Se corrigió error

--- crud/editar.php
+++ crud/editar.php
@@ -1,8 +1,10 @@
 <?php
 require __DIR__ . '/includes/funciones.php';
 
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     editar_usuario($_POST);
+    header("Location: index.php");
+    exit;
 }
 
 $usuarios = obtener_usuarios();
@@ -22,6 +24,7 @@ $usuarios = obtener_usuarios();
 
 <div class="container">
 <form method="POST" name="formUsuario" onsubmit="return validarFormulario();">
+
     <select name="id" required>
         <?php while($u = mysqli_fetch_assoc($usuarios)): ?>
             <option value="<?php echo $u['id']; ?>">
@@ -33,8 +36,18 @@ $usuarios = obtener_usuarios();
     <input name="nombre" placeholder="Nuevo nombre" required>
     <input name="apellido" placeholder="Nuevo apellido" required>
     <input name="email" placeholder="Nuevo email" required>
+    <input name="telefono" placeholder="Nuevo teléfono">
+    <input name="direccion" placeholder="Nueva dirección">
+    <input name="ciudad" placeholder="Nueva ciudad">
+    <input name="pais" placeholder="Nuevo país">
+    <input type="date" name="fecha">
 
-    <button>Actualizar</button>
+    <select name="genero">
+        <option value="Masculino">Masculino</option>
+        <option value="Femenino">Femenino</option>
+    </select>
+
+    <button type="submit">Actualizar</button>
 </form>
 </div>
 
